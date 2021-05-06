b402ee35e52ef25be9edcde54c46b8ec0f8ee42b
melissakipp <melissakipp.az@gmail.com>
2021-05-06 12:01:28 -0700

    file clean up

--- index.js
+++ index.js
@@ -1,37 +1,19 @@
-const express = require('express');
+const express = require("express");
 const app = express();
 
-app.get('/', (req, res) => res.send('Hello World. And this is WORKING.'))
-app.listen(3000, () => console.log('Server ready'))
-
-// // catch 404 and forward to error handler
-// app.use(function(req, res, next) {
-//   var err = new Error('Not Found');
-//   err.status = 404;
-//   next(err);
-// });
-
-// // error handler
-// app.use(function(err, req, res) {
-//   // set locals, only providing error in development
-//   res.locals.message = err.message;
-//   res.locals.error = req.app.get('env') === 'development' ? err : {};
-
-//   // render the error page
-//   res.status(err.status || 500);
-//   res.render('error');
-// });
+app.get("/", (req, res) => res.send("Hello World."));
+app.listen(3000, () => console.log("Server ready"));
 
 // Bret Fisher: Docker for Node.js course Video 25
 // quit on ctrl-c when running docker in terminal
-process.on('SIGINT', function onSigint () {
-	console.info('Got SIGINT (aka ctrl-c in docker). Graceful shutdown ', new Date().toISOString());
+process.on("SIGINT", function onSigint () {
+	console.info("Got SIGINT (aka ctrl-c in docker). Graceful shutdown ", new Date().toISOString());
   shutdown();
 });
 
 // quit properly on docker stop
-process.on('SIGTERM', function onSigterm () {
-  console.info('Got SIGTERM (docker container stop). Graceful shutdown ', new Date().toISOString());
+process.on("SIGTERM", function onSigterm () {
+  console.info("Got SIGTERM (docker container stop). Graceful shutdown ", new Date().toISOString());
   shutdown();
 })
 
@@ -50,11 +32,3 @@ function shutdown() {
 // END Bret Fisher
 
 module.exports = app;
-
-app.get("/search", (req,res) => {
-  const { q } = req.query;
-  if (!q) {
-    res.sent("Nothing found if nothing searched.")
-  }
-  res.send(`<h1>Search results for: ${ q }</h1>`)
-});
